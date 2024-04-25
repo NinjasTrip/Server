1c0cc49358b36bb6846edbe7f48adc7273598719
강병헌 <fund88@naver.com>
2024-04-25 17:36:42 +0900

    first commit

---
+++ src/main/java/com/ninjatrip/NinjaTripApplication.java
@@ -0,0 +1,13 @@
+package com.ninjatrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninjatrip/NinjaTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjatrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
