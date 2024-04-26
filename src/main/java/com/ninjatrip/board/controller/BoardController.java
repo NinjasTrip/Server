9384d7a5462fdff7fb97d0aea0467839bccce698
seongddiyong <kodd1102@naver.com>
2024-04-26 17:25:44 +0900

    [#12] refactor: modify Board return types in `BoardController.java`

--- src/main/java/com/ninjatrip/board/controller/BoardController.java
+++ src/main/java/com/ninjatrip/board/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.ninjatrip.board.controller;
 import com.ninjatrip.board.dto.Board;
 import com.ninjatrip.board.dto.BoardResponseDto;
 import com.ninjatrip.board.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +20,57 @@ public class BoardController {
     }
 
     @GetMapping
-    public List<Board> getBoardList() {
-        return boardService.getBoardList();
+    public ResponseEntity<?> getBoardList() {
+        try {
+            List<Board> boards =  boardService.getBoardList();
+            return ResponseEntity.status(HttpStatus.OK).body(boards);
+        } catch (Exception e) {
+            return exceptionHandler(e);
+        }
+
     }
 
     @GetMapping("/{boardIdx}")
-    public BoardResponseDto getBoard(@PathVariable("boardIdx") int boardIdx) {
-        return boardService.getBoard(boardIdx);
+    public ResponseEntity<?> getBoard(@PathVariable("boardIdx") int boardIdx) {
+        try {
+            BoardResponseDto board = boardService.getBoard(boardIdx);
+            return ResponseEntity.status(HttpStatus.OK).body(board);
+        } catch (Exception e) {
+            return exceptionHandler(e);
+        }
     }
 
     @PostMapping
-    public void writeBoard(@RequestBody Board board) {
-        boardService.writeBoard(board);
+    public ResponseEntity<?> writeBoard(@RequestBody Board board) {
+        try {
+            boardService.writeBoard(board);
+            return ResponseEntity.status(HttpStatus.OK).body("작성 완료");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("작성 실패");
+        }
     }
 
     @PatchMapping
-    public void modifyBoard(@RequestBody Board board) {
-        boardService.modifyBoard(board);
+    public ResponseEntity<?> modifyBoard(@RequestBody Board board) {
+        try {
+            boardService.modifyBoard(board);
+            return ResponseEntity.status(HttpStatus.OK).body("수정 완료");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("수정 실패");
+        }
     }
 
     @DeleteMapping("/{boardIdx}")
-    public void deleteBoard(@PathVariable("boardIdx") int boardIdx) {
-        boardService.deleteBoard(boardIdx);
+    public ResponseEntity<?> deleteBoard(@PathVariable("boardIdx") int boardIdx) {
+        try {
+            boardService.deleteBoard(boardIdx);
+            return ResponseEntity.status(HttpStatus.OK).body("삭제 완료");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("삭제 실패");
+        }
     }
 
+    private ResponseEntity<String> exceptionHandler(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+	}
 }
