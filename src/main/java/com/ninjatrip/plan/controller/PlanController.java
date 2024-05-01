f5c1826cec83a0cb043c73dc1ba404291195bc47
BH kang <rkdqudgjs1127@gmail.com>
2024-05-01 22:53:22 +0900

    [#17] Fix : Plan Controller 수정

--- src/main/java/com/ninjatrip/plan/controller/PlanController.java
+++ src/main/java/com/ninjatrip/plan/controller/PlanController.java
@@ -1,5 +1,7 @@
 package com.ninjatrip.plan.controller;
 
+import com.ninjatrip.plan.dto.Plan;
+import com.ninjatrip.user.dto.Token;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -8,32 +10,32 @@ import org.springframework.http.ResponseEntity;
 public class PlanController {
 
     @PostMapping
-    public ResponseEntity<String> createPlan(@RequestBody String planDetails) {
+    public ResponseEntity<String> createPlan(Plan plan , Token token) {
         // 여행 계획 생성 로직 구현
         return ResponseEntity.ok("Plan created successfully");
     }
 
     @PostMapping("/{planIdx}")
-    public ResponseEntity<String> setPlanRoute(@PathVariable String planIdx, @RequestBody String routeDetails) {
+    public ResponseEntity<String> setPlanRoute(Plan plan , Token token) {
         // 여행 경로 설정 로직 구현
-        return ResponseEntity.ok("Route for plan " + planIdx + " set successfully");
+        return ResponseEntity.ok("Route for plan " + plan.getPlanId() + " set successfully");
     }
 
     @PatchMapping("/{planIdx}")
-    public ResponseEntity<String> modifyPlan(@PathVariable String planIdx, @RequestBody String newPlanDetails) {
+    public ResponseEntity<String> modifyPlan(Plan plan , Token token) {
         // 여행 계획 수정 로직 구현
-        return ResponseEntity.ok("Plan " + planIdx + " updated successfully");
+        return ResponseEntity.ok("Plan  updated successfully");
     }
 
     @DeleteMapping("/{planIdx}")
-    public ResponseEntity<String> deletePlan(@PathVariable String planIdx) {
+    public ResponseEntity<String> deletePlan(Plan plan , Token token) {
         // 여행 계획 삭제 로직 구현
-        return ResponseEntity.ok("Plan " + planIdx + " deleted successfully");
+        return ResponseEntity.ok("Plan deleted successfully");
     }
 
     @GetMapping("/{planIdx}")
-    public ResponseEntity<String> viewPlan(@PathVariable String planIdx) {
+    public ResponseEntity<String> viewPlan(Plan plan , Token token) {
         // 여행 계획 조회 로직 구현
-        return ResponseEntity.ok("Details for plan " + planIdx);
+        return ResponseEntity.ok("Details for plan planIdx");
     }
 }
